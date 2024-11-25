99a092cfe07294a619b12dfecacd91ad3a7fac1e
Lynx <jayruizdeocampo11503@gmail.com>
2024-11-25 22:55:36 +0800

    Portfolio Website first commit

---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>My Portfolio</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/navbar.css">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100..900;1,100..900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+<body>
+    <header>
+        <nav class="navbar">
+            <div class="navdiv">
+                <div class="logo">
+                    <a href="#">
+                        <img src="assets/images/Logo.png" alt="My Logo">
+                    </a>
+                </div>
+                <ul class="navmenu">
+                    <li class="navmenu_items">
+                        <a href="#">Home</a>
+                    </li>
+                    <li class="navmenu_items">
+                        <a href="#">About Me</a>
+                    </li>
+                    <li class="navmenu_items">
+                        <a href="#">Skills</a>
+                    </li>
+                    <li class="navmenu_items">
+                        <a href="#">Portfolio</a>
+                    </li>
+                    <li class="navmenu_itemsBtn">
+                        <a href="#">Contact Me</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+
+        <div class="slanted-edge">
+            <img src="assets/images/Avatar.png" alt="My Avatar" class="avatar">
+        </div>
+
+        <div class="landingPageTxt">
+            <p class="lPTxt1">Hi, I am</p>
+            <div class="text-container">
+                <h1 class="lPTxt2">Jay Ruiz De Ocampo</h1>
+                <p class="lPTxt3">BS Computer Engineering Student / Game Developer / Streamer</p>
+                <div class="social_buttons">
+                    <div class="outer_circle">
+                        <img class="github_logo" src="assets/images/Github_Logo.png" alt="GitHub Logo">
+                    </div>
+                    <div class="outer_circle">
+                        <img class="linkedin_logo" src="assets/images/LinkedIn_Logo.png" alt="LinkedIn Logo">
+                    </div>
+                    <div class="outer_circle">
+                        <img class="facebook_logo" src="assets/images/Facebook_Logo.png" alt="Facebook Logo">
+                    </div>
+                    <div class="outer_circle">
+                        <img class="tiktok_logo" src="assets/images/Tiktok_Logo.png" alt="TikTok Logo">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+</body>
+</html>
