4d0560be1dc5efe1a8b3076aea464109b279c298
Lynx <jayruizdeocampo11503@gmail.com>
2024-11-26 16:49:47 +0800

    Navbar menu fill

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
     <link rel="stylesheet" href="css/navbar.css">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Raleway:ital,wght@0,100..900;1,100..900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Raleway:ital,wght@0,100..900;1,100..900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
 
 <body>
     <header>
@@ -18,19 +19,11 @@
                     </a>
                 </div>
                 <ul class="navmenu">
-                    <li class="navmenu_items">
-                        <a href="#">Home</a>
-                    </li>
-                    <li class="navmenu_items">
-                        <a href="#">About Me</a>
-                    </li>
-                    <li class="navmenu_items">
-                        <a href="#">Skills</a>
-                    </li>
-                    <li class="navmenu_items">
-                        <a href="#">Portfolio</a>
-                    </li>
-                        <a href="#" class="bn5">Hover</a>
+                    <li class="navmenu_items"><a href="#">Home</a></li>
+                    <li class="navmenu_items"><a href="#">About Me</a></li>
+                    <li class="navmenu_items"><a href="#">Skills</a></li>
+                    <li class="navmenu_items"><a href="#">Portfolio</a></li>
+                    <a href="#" class="bn5">Contact Me</a>
                 </ul>
                 
             </div>
